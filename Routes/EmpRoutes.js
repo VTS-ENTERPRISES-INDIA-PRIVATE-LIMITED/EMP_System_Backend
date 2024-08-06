275b7d057862baedbba140349900e316c91e2dd8
Ravi Kiran <113467994+RaviKiran25052@users.noreply.github.com>
2024-08-06 14:21:12 +0530

    leave

---
+++ Routes/EmpRoutes.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const router = express.Router();
+const connection = require('../db');
+
+router.post('/addEmp',async(req,res)=>{
+  try{
+    const createEmptable = `
+  CREATE TABLE IF NOT EXISTS employee (
+    empId VARCHAR(255),
+    Name VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(255),
+    password VARCHAR(255),
+    role VARCHAR(255)
+  )
+  `
+  await connection.query(createEmptable)
+  
+ 
+  const empdata = req.body
+  
+  for(var i=0;i<empdata.length;i++)
+  {
+    const {empId,name,email,phone,role} = empdata[i]
+    const query = `
+    INSERT INTO employee (empId,Name,email,phone,password,role) VALUES(?,?,?,?,?,?)
+  `
+  connection.query(query,[empId,name,email,phone,empId,role])
+  .then(resp=>console.log(`${name} added successfully`))
+  .catch(err=>console.log(`error occured with ${name}`))
+  }
+  res.send("Employee data added successfully")
+}
+catch(err){
+  console.log(err)
+  res.status(400).send("Error Adding Data")
+}
+})
+  router.post('/viewEmp', async (req, res) => {
+    const query = "SELECT * FROM employee"
+    const data = await connection.query(query)
+    res.send(data)
+  })
+  router.post('/viewEmp/:id', async (req, res) => {
+    const id = req.params.id
+    const query = "SELECT * FROM employee WHERE empId = ?"
+    const data = await connection.query(query, [id])
+    res.send(data)
+  })
+  router.post('/updateEmp/:id', async (req, res) => {
+    const id = req.params.id
+    const query1 = "SELECT * FROM employee WHERE empId = ?"
+    const existId = await connection.query(query1, [id])
+    if(existId[0].length){
+      const { Name, email, phone, password, role } = req.body;
+      const query = "UPDATE employee SET Name = ?, email = ?, phone = ?, password = ?, role = ? WHERE empId = ?"
+      const updatedData = await connection.query(query, [Name, email, phone, password, role, id])
+      if(updatedData[0].affectedRows) {
+        res.status(200).send("User Updated Successfully...!")
+      } else {
+        res.status(500).send("Error occured..!")
+      }
+    } else{
+      res.status(404).send("Employee does not exist")
+    }
+  })
+  router.post('/deleteEmp/:id', async (req, res)=>{
+    const id = req.params.id
+    const query1 = "SELECT * FROM employee WHERE empId = ?"
+    const existId = await connection.query(query1, [id])
+    
+    if(existId[0].length){
+      const query = "DELETE FROM employee WHERE empId = ?"
+      const deletedData = await connection.query(query, [id])
+      if(deletedData[0].affectedRows){
+        res.status(200).send("User deleted successfully")
+      }
+      else{
+        res.status(500).send("Error occured")
+      }
+    }
+    else{
+      res.status(404).send("Employee does not exist")
+    }
+  })
+
+module.exports = router;
--- sample.js
+++ sample.js
@@ -6,7 +6,8 @@ require('./db')
 require('dotenv').config();
 
 // const admin = require('./Routes/adminRoutes');
-const admin = require('./Routes/leaveRoutes');
+const admin = require('./Routes/EmpRoutes');
+const leave = require('./Routes/leaveRoutes');
 const app = express();
 
 app.use(bodyParser.json());
@@ -17,6 +18,7 @@ app.get('/',(req,res)=>{
     res.send("Hello Guru");
 });
 app.use('/emp',employeeRoutes)
+app.use('/leave',leave)
 app.use('/admin',admin);
 
 app.listen(port,()=>{
